126db33fc1defd784b90a243e656335bdf2d169c
miguruiz <miguruiz@expediagorup.com>
2023-05-03 13:32:40 +0200

    modified serialization type for weather to avro

--- producers/models/weather.py
+++ producers/models/weather.py
@@ -73,7 +73,7 @@ class Weather(Producer):
 
         resp = requests.post(
            f"{Weather.rest_proxy_url}/topics/{self.topic_name}",
-           headers={"Content-Type":  "application/vnd.kafka.json.v2+json"},
+           headers={"Content-Type":  "application/vnd.kafka.avro.v2+json"},
            data=json.dumps(
                {"value_schema": json.dumps(Weather.value_schema),
                 "key_schema": json.dumps(Weather.key_schema),
